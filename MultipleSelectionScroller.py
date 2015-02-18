dd092793fbf3c64b64b4a8c246f9c04fb17c8fab
mattst <mattst@i-dig.info>
2015-02-18 22:26:35 +0000

    Additions to documentation and comment lines.

--- MultipleSelectionScroller.py
+++ MultipleSelectionScroller.py
@@ -9,7 +9,7 @@
 #
 # Written by:     Matthew Stanfield
 #
-# Last Edited:    2015-02-17
+# Last Edited:    2015-02-18
 #
 # Version:        n/a
 #
@@ -30,12 +30,12 @@
 
 import sublime
 import sublime_plugin
-import math
 
 
 class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
     """
     The MultipleSelectionScrollerCommand class is a Sublime Text plugin which...
+
     """
 
 
@@ -86,7 +86,7 @@ class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
         else:
             return scroll_direction_default
 
-        # Warning to display as a status message and in the console if 'forward' is not boolean.
+        # Warning to display as a status message and in the console if "forward" is not boolean.
         msg =  "multiple_selection_scroller command: 'forward' arg can be set to 'true' or 'false'"
 
         # If not set to a boolean value - display warning and return the default.
@@ -113,33 +113,47 @@ class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
 
 
     def handle_scrolling(self, scroll_direction):
+        """
+        handle_scrolling() calls either the scroll_forwards() or the scroll_backwards() method
+        according to the value of its scroll_direction argument. It provides these methods with
+        their essential shared requirement, the region of the middle line of the visible region.
 
-        # Scrolling forwards through the selections is done by moving the next selection which
-        # occurs after the middle line of the visible region to the middle line of the visible
-        # region. Scrolling backwards through the selections is done in the same way except by
-        # moving the next selection which occurs before the middle line.
+        This method also provides important notes about how this plugin handles the scrolling.
+        """
+
+        # Scrolling forwards through the selections is done by moving the first selection to occur
+        # below the middle line of the visible region to the middle line of the visible region.
         #
-        # It is not possible to do this in two cases:
+        # Scrolling backwards through the selections is done by moving the first selection to occur
+        # above the middle line of the visible region to the middle line of the visible region.
         #
-        # If a selection is near the top of the buffer, Sublime Text won't scroll the line it is
-        # on to the centre of the visible region, there is no 'scroll_above_begining' setting.
+        # Repeated pressing of the command's keys allow scrolling backwards and forwards through all
+        # the selections.
         #
-        # If a selection in near the bottom of the buffer and the setting 'scroll_past_end' has been
-        # set to false (defaults to true) then again Sublime Text won't move the line it is on to
-        # the centre of the visible region (although it will if 'scroll_past_end' is set to true).
+        # Scrolling is performed by calling view.show_at_center(point) - there are two cases when
+        # Sublime Text will not honour calls made to that method and move the visible region:
         #
-        # In both of these cases, although the selection will not be moved to the middle line of
-        # the visible region, the selection will still be viewable on the screen.
+        # 1) If a selection is near the top of the buffer, Sublime Text won't scroll the line it is
+        # on to the centre of the visible region, there is no 'scroll_above_beginning' setting.
+        #
+        # 2) If a selection in near the bottom of the buffer and the setting 'scroll_past_end' has
+        # been set to false (defaults to true) then again Sublime Text won't move the line it is on
+        # to the centre of the visible region (however it will if 'scroll_past_end' is set to true).
+        #
+        # In both of these cases the next/previous selection will not be moved to the middle line
+        # of the visible region, however all remaining selections whether below the middle line (if
+        # scrolling forwards) or above the middle line (if scrolling backwards) will definitely be
+        # in the visible region on the screen.
 
         # Get the visible region, the list of visible lines, and the number of visible lines.
         visible_region = self.view.visible_region()
         visible_lines = self.view.lines(visible_region)
         visible_lines_len = len(visible_lines)
 
-        # Just in case nothing is visible at all. Note that view.lines() will always return at least
-        # one line (the top line) even if the visible_region is (0, 0). Note also that if two lines
-        # are visible then the scrolling done by this plugin still works.
-        if visible_lines_len < 2:
+        # Abort in the unlikely event that fewer than 2 lines are visible.
+        visible_lines_minimum = 2
+
+        if visible_lines_len < visible_lines_minimum:
             return
 
         # Calculate which line is in the middle of the visible lines, converting to int floors it.
@@ -160,13 +174,18 @@ class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
 
 
     def scroll_forwards(self, middle_line):
+        """
+        scroll_forwards() moves the visible region to centre on the first selection to occur below
+        the middle_line region, if there is such a selection. See the lengthy comment notes in
+        handle_scrolling() about why Sublime Text may not honour calls to view.show_at_center().
+        """
 
-        # Store the selections and the number of them.
+        # Get the selections and the number of them.
         sels = self.view.sel()
         sels_len = len(sels)
 
-        # Starting from the first selection, loop through all the selections looking for the next
-        # selection to occur after the middle line.
+        # Starting at the first selection, loop forwards through all the selections looking for the
+        # first selection to occur below the middle line.
 
         sel_index = 0
         found = False
@@ -175,7 +194,7 @@ class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
 
             sel = sels[sel_index]
 
-            # If a selection is found after the middle line.
+            # If a selection is found below the middle line.
             if sel.begin() > middle_line.end():
 
                 # Scroll the visible region to the line the selection begins on.
@@ -190,13 +209,18 @@ class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
 
 
     def scroll_backwards(self, middle_line):
+        """
+        scroll_backwards() moves the visible region to centre on the first selection to occur above
+        the middle_line region, if there is such a selection. See the lengthy comment notes in
+        handle_scrolling() about why Sublime Text may not honour calls to view.show_at_center().
+        """
 
-        # Store the selections and the number of them.
+        # Get the selections and the number of them.
         sels = self.view.sel()
         sels_len = len(sels)
 
-        # Starting from the last selection, loop through all the selections looking for the next
-        # selection to occur before the middle line.
+        # Starting at the last selection, loop backwards through all the selections looking for the
+        # first selection to occur above the middle line.
 
         sel_index = sels_len - 1
         found = False
@@ -205,7 +229,7 @@ class MultipleSelectionScrollerCommand(sublime_plugin.TextCommand):
 
             sel = sels[sel_index]
 
-            # If a selection is found before the middle line.
+            # If a selection is found above the middle line.
             if sel.end() < middle_line.begin():
 
                 # Scroll the visible region to the line the selection begins on.
